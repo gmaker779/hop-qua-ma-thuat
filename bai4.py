dade3ca7ee0d706ef8e381379f06141802836f7a
gmaker779 <gmaker643@gmail.com>
2025-10-03 13:33:03 +0000

    bai4

--- bai4.py
+++ bai4.py
@@ -1,8 +1,8 @@
 from guizero import Picture, App, Text
 
 cuaso = App(title="Thiệp chúc mừng", width=1920, height=1024, bg="lightgray")
-chu = Text(cuaso, "Chúc mừng sinh nhật", size=13, color = 'black')
+chu = Text(cuaso, "Chúc mừng sinh nhật", color = 'black')
 hinhanh = Picture(cuaso, image="birthday.png")
-chu = Text(cuaso, "Từ: Thăng", size=13)
+chu = Text(cuaso, "Từ: Thăng")
 
 cuaso.display()
